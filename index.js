7de4c71ffb310db20452676762b394f3d338d6a1
Jiyong Kim <luke.jiyongkim@gmail.com>
2023-03-24 10:37:56 -0400

    completed lab

---
+++ index.js
@@ -0,0 +1,10 @@
+function receivesAFunction(callBack) {
+    callBack();
+}
+function returnsANamedFunction() {
+   return function namedFunction() {};
+}
+
+function returnsAnAnonymousFunction() {
+    return function () {};
+}
